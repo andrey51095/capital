0c0b45a9bf322d91ec6e5f3fd9d71a25a68393dd
Andrei Liavon <andrey51095@gmail.com>
2022-08-29 15:38:27 +0300

    fix build

--- packages/client/src/pages/money-summary/index.js
+++ packages/client/src/pages/money-summary/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyledSpinnerNext} from 'baseui/spinner';
+import {Spinner} from 'baseui/spinner';
 import {Block} from 'baseui/block';
 import {useQuery} from 'react-apollo';
 
@@ -13,7 +13,7 @@ const MoneySummary = () => {
   const {data, loading} = useQuery(QUERY_MONEY_SUMMARY);
 
   if (loading) {
-    return <StyledSpinnerNext />;
+    return <Spinner />;
   }
 
   return (
--- packages/client/src/pages/money-summary/summary-in-pln.jsx
+++ packages/client/src/pages/money-summary/summary-in-pln.jsx
@@ -1,50 +1,67 @@
-import React, { useMemo} from 'react'
-import { Block } from 'baseui/block'
-import { Spinner } from 'baseui/spinner';
+import React, {useMemo} from 'react';
+import {Block} from 'baseui/block';
+import {Spinner} from 'baseui/spinner';
 
 import Currency from '../../components/currency';
 import {useFetchAll} from '../../hooks';
 
 const PLN = 'PLN';
 
-const getUrl = (code) => `http://api.nbp.pl/api/exchangerates/rates/C/${code}`;
+const getUrl = code => `http://api.nbp.pl/api/exchangerates/rates/C/${code}`;
 
-var myHeaders = new Headers();
-var requestOptions = {
+let myHeaders = new Headers();
+let requestOptions = {
   method: 'GET',
-  headers: myHeaders
+  headers: myHeaders,
 };
 
-export default function SummaryInPln({ summary }) {
-  const urls = useMemo(() => summary.map(({ currency }) => currency).filter(c => c !== PLN).map(getUrl), [summary]);
-  const { data, loading } = useFetchAll(urls, requestOptions)
+export default function SummaryInPln({summary}) {
+  const urls = useMemo(() => summary.map(({currency}) => currency).filter(c => c !== PLN)
+    .map(getUrl), [summary]);
+  const {data, loading} = useFetchAll(urls, requestOptions);
 
-  const preparedData = data.reduce((acc, item) => ({ [item.code]: item.rates[0], ...acc }), {})
+  const preparedData = data.reduce((acc, item) => ({
+    [item.code]: item.rates[0],
+    ...acc,
+  }), {});
 
-  const totalValue = data.length ? summary.reduce((acc, {currency, amount}) => acc + (amount * (preparedData[currency]?.bid || 1)), 0) : 0
-
+  const totalValue = data.length ? summary.reduce((acc, {currency, amount}) => acc + (amount * (preparedData[currency]?.bid || 1)), 0) : 0;
 
   return (
-    <Block display="flex" marginTop="scale500" justifyContent="center" alignItems="center" gridColumnGap="scale400">
+    <Block
+      display="flex"
+      marginTop="scale500"
+      justifyContent="center"
+      alignItems="center"
+      gridColumnGap="scale400"
+    >
       {loading && <Spinner />}
 
       {!loading && (
         <Block>
-        <Block display="flex" >
-          ~{Math.round(totalValue)}
-          (<Currency value={PLN} />)
-        </Block>
+          <Block display="flex" >
+            ~
+            {Math.round(totalValue)}
+            (
+            <Currency value={PLN} />
+            )
+          </Block>
           <Block paddingLeft="scale200">
-            {Object.keys(preparedData).map((key) => (<Block key={ key} display="flex" gridColumnGap="scale200">
-              <Currency value={key} />
-              <Block>{preparedData[key].effectiveDate }</Block>
-              <Block>{preparedData[key].bid }</Block>
-              <Block>{preparedData[key].ask }</Block>
-            </Block>))}
+            {Object.keys(preparedData).map(key => (
+              <Block
+                key={key}
+                display="flex"
+                gridColumnGap="scale200"
+              >
+                <Currency value={key} />
+                <Block>{preparedData[key].effectiveDate }</Block>
+                <Block>{preparedData[key].bid }</Block>
+                <Block>{preparedData[key].ask }</Block>
+              </Block>
+            ))}
           </Block>
         </Block>
-      )
-      }
-   </Block>
- )
+      )}
+    </Block>
+  );
 }
