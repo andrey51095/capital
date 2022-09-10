ada5d8a0f897f4e0745ed80451248661020c11ad
Andrei Liavon <andrey51095@gmail.com>
2022-09-11 00:55:52 +0300

    add useMoneyBundles hook

---
+++ packages/client/src/hooks/use-money-bundles.js
@@ -0,0 +1,31 @@
+// @flow
+import gql from 'graphql-tag';
+import {useQuery} from 'react-apollo';
+
+const QUERY_MONEY_BUNDLES = gql`
+  query MoneyBundles {
+    moneyBundles {
+      id
+      currency
+      description
+      amount
+      storage
+      type
+      createdAt
+      updatedAt
+      deletedAt
+    }
+  }
+`;
+
+const useMoneyBundles = () => {
+  const {data, loading, error, refetch} = useQuery(QUERY_MONEY_BUNDLES);
+
+  return {
+    data,
+    loading,
+    error,
+    refetch,
+  };
+};
+export default useMoneyBundles;
