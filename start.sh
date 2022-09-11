34cd6ee8b205adc707fc11ad624d201ca67d36bc
Andrei Liavon <andrey51095@gmail.com>
2022-09-11 13:05:50 +0300

    update startup docker config for --dev

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Starting docker-compose..."
-if [ "$1" == "--dev" ]; 
+if [ "$1" == "--dev" ];
   then echo "Starting dev version of the app..."
-       docker-compose -f docker-compose.dev.yml down && docker-compose -f docker-compose.dev.yml up -d --build  
+       docker-compose -f docker-compose.dev.yml down && docker-compose -f docker-compose.dev.yml up -d --build
   else docker-compose down && docker-compose up -d --build
 fi
 echo "App is running!"
