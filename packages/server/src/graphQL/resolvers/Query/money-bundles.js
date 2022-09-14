c011b3ca158ba052a8e87582d0063b33819d9104
Andrei Liavon <andrey51095@gmail.com>
2022-09-14 17:36:50 +0300

    [BE] Add delete money bundle resolver

--- packages/server/src/graphQL/resolvers/Query/money-bundles.js
+++ packages/server/src/graphQL/resolvers/Query/money-bundles.js
@@ -1,7 +1,7 @@
 
 module.exports = async (_, _args, context, _info) => {
   const {MoneyBundle} = context.schemas;
-  const bundles = await MoneyBundle.find({});
+  const bundles = await MoneyBundle.find({deletedAt: 0});
 
   return bundles;
 };
