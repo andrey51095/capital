10026abc5479321447487d58ab8eaf6ea2503266
Andrei Liavon <andrey51095@gmail.com>
2022-09-18 15:20:31 +0300

    [FE] Add SummaryCard component

---
+++ packages/client/src/hooks/graphql/query/use-money-summary.js
@@ -0,0 +1,27 @@
+// @flow
+import {useMemo} from 'react';
+import {useQuery} from 'react-apollo';
+import gql from 'graphql-tag';
+
+const QUERY_MONEY_SUMMARY = gql`
+  query MoneySummary {
+    summary: moneySummary {
+      amount
+      currency
+    }
+  }
+`;
+
+const useMoneySummary = () => {
+  const {data, loading, error, refetch} = useQuery(QUERY_MONEY_SUMMARY);
+
+  const summary = useMemo(() => ([...(data?.summary || [])]), [data]);
+
+  return {
+    summary,
+    loading,
+    error,
+    refetch,
+  };
+};
+export default useMoneySummary;
